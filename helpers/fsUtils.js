ed9bfb9099d9eb40ab1959735e7c518f67bdd1a2
slayonce <katharinemg@gmail.com>
2023-06-03 18:27:01 -0400

    added routes/notes.js and helpers/fsUtils.js files

---
+++ helpers/fsUtils.js
@@ -0,0 +1,14 @@
+// SECTION: IMPORT MODULES
+const fs = require('fs');
+const util = require('util');
+
+// SECTION: READ FILE
+
+
+// SECTION: WRITE TO FILE
+
+
+// SECTION: READ AND APPEND
+
+
+// SECTION: EXPORT MODULE
---
+++ public/routes/notes.js
@@ -0,0 +1,7 @@
+// SECTION: ROUTER
+const notes = require('express').Router();
+
+// SECTION: GET route for retrieving notes
+
+// SECTION: POST Route for submitting notes
+
